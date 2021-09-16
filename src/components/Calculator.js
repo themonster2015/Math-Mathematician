f6f8e106b4ebea3a97bdbf969e1b9204ca33f0a8
themonster2015 <quynhyen.vothi@gmail.com>
2021-09-16 10:39:53 +0200

    refactored Calculator to use Hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,63 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import calculate from './logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-  }
+export default function Calculator() {
+  const elements = {
+    total: 0,
+    next: null,
+    operation: null,
+  };
+  const [status, setStatus] = useState(elements);
+  const clickHandler = (e) => {
+    setStatus({ ...status, ...calculate(status, e.target.innerText) });
+  };
 
-   handleClick = (e) => {
-     // eslint-disable-next-line react/no-access-state-in-setstate
-     this.setState(calculate(this.state, e.target.innerText));
-   }
+  return (
+    <div className="wrapper">
+      <div className="result">
+        {status.total}
+        {' '}
+        {status.operation}
+        {' '}
+        {status.next}
+      </div>
+      <button type="button" onClick={clickHandler} className="greyBtn">AC</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">+/-</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">%</button>
+      <button type="button" onClick={clickHandler} className="orangeBtn">÷</button>
 
-   render() {
-     const { total, next, operation } = this.state;
-     console.log(this.state);
-     return (
-       <div className="wrapper">
-         <div className="result">
-           {total}
-           {' '}
-           {operation}
-           {' '}
-           {next}
-         </div>
-         <button type="button" onClick={this.handleClick} className="greyBtn">AC</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">+/-</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">%</button>
-         <button type="button" onClick={this.handleClick} className="orangeBtn">÷</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">7</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">8</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">9</button>
+      <button type="button" onClick={clickHandler} className="orangeBtn">x</button>
 
-         <button type="button" onClick={this.handleClick} className="greyBtn">7</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">8</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">9</button>
-         <button type="button" onClick={this.handleClick} className="orangeBtn">x</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">4</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">5</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">6</button>
+      <button type="button" onClick={clickHandler} className="orangeBtn">-</button>
 
-         <button type="button" onClick={this.handleClick} className="greyBtn">4</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">5</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">6</button>
-         <button type="button" onClick={this.handleClick} className="orangeBtn">-</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">1</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">2</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">3</button>
+      <button type="button" onClick={clickHandler} className="orangeBtn">+</button>
 
-         <button type="button" onClick={this.handleClick} className="greyBtn">1</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">2</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">3</button>
-         <button type="button" onClick={this.handleClick} className="orangeBtn">+</button>
+      <button type="button" onClick={clickHandler} className="greyBtn zero">0</button>
+      <button type="button" onClick={clickHandler} className="greyBtn">.</button>
+      <button type="button" onClick={clickHandler} className="orangeBtn">=</button>
 
-         <button type="button" onClick={this.handleClick} className="greyBtn zero">0</button>
-         <button type="button" onClick={this.handleClick} className="greyBtn">.</button>
-         <button type="button" onClick={this.handleClick} className="orangeBtn">=</button>
+    </div>
 
-       </div>
-
-     );
-   }
+  );
 }
-
-export default Calculator;
